ff02b29a2a75583dfc2b8d157e59d0050e838d9f
ihsan <ihsanl@pm.me>
2022-03-05 16:57:12 +0300

    update so much things

--- dark-light.sh
+++ dark-light.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# <swiftbar.hideAbout>true</swiftbar.hideAbout>
-# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
-# <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
-# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
-# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
+# # <swiftbar.hideAbout>true</swiftbar.hideAbout>
+# # <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
+# # <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
+# # <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
+# # <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
+# echo 'dl | refresh=true'
 
-echo 'dl | refresh=true'
+lightbg='#A7C080'
+lightbg='#CDA863'
+darkbg='#2D3339'
+darkbg='#16171F'
 
+bg=
 target=
 if defaults read -g AppleInterfaceStyle | grep -q Dark; then
 	target=light
+	bg="$lightbg"
 else
 	target=dark
+	bg="$darkbg"
 fi
+
 
 osascript -e 'tell application "System Events"
 	if dark mode of appearance preferences is false then
@@ -25,6 +33,7 @@ end tell'
 
 sed -i '' "s/^colors: \*[darklight]*$/colors: *$target/" /Users/ihsan/.config/alacritty/color.yml
 
-kitty @ set-colors --all --configured "/Users/ihsan/.config/kitty/everforest-$target-kitty.conf" 2>&1 >/tmp/kitt.log
+which -s kitty && kitty @ set-colors --all --configured "/Users/ihsan/.config/kitty/everforest-$target-kitty.conf" 2>&1 >/tmp/kitt.log
+m wallpaper $(echo "$bg" | ~/bin/hex2bmp)
 
 for pid in $(pgrep {n,}vim); do kill -SIGUSR1 $pid; done
--- plot
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# nix-shell
-# nix-shell -p gnuplot
-
-gnuplot -persist <(echo "plot '<(sort -n listOfNumbers.txt)' with lines")
-
-# gnuplot -p -e 'set terminal png enhanced truecolor; set output "out.png"; plot "/dev/stdin" using 1:2'
-# >&2 echo 'created: out.png'
